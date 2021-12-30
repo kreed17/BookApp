efcf63c6709942ebf512565e3ee769a0ad21c595
kreed17 <70284103+kreed17@users.noreply.github.com>
2021-12-30 16:36:58 -0600

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,26 @@
+import React, {Component} from 'react';
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import './App.css';
+
+import CreateBook from './components/CreateBook';
+import ShowBookList from './components/ShowBookList';
+import ShowBookDetails from './components/ShowBookDetails';
+import UpdateBookInfo from './components/UpdateBookInfo';
+
+
+class App extends Component{
+  render() {
+    return (
+      <Router>
+        <div>
+          <Route exact path='/' component={ShowBookList} />
+          <Route exact path='/create-book' component={CreateBook} />
+          <Route exact path='/edit-book/:id' component={UpdateBookInfo} />
+          <Route exact path='/show-book/:id' component={ShowBookDetails} />
+        </div>
+      </Router>
+    );
+  }
+}
+
+export default App;
---
+++ src/components/Footer.js
@@ -0,0 +1,10 @@
+import React from "react";
+import '../App.css';
+
+const Footer = () => (
+  <div className="footer">
+    <p>CopyRight @ Kamron Reed 2021</p>
+  </div>
+);
+
+export default Footer;
